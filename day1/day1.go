e4395bdc37822c6237252fb7a7481af4af93f4b3
cijin <cijininurhood@gmail.com>
2023-09-17 17:55:50 +0700

    solve day1 part1 & part2

--- day1/day1.go
+++ day1/day1.go
@@ -1,1 +1,62 @@
 package day1
+
+import (
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func parseCalories(lines []string) ([][]int, error) {
+	var out [][]int
+	var temp []int
+
+	for i, line := range lines {
+		if line == "" || i+1 == len(lines) {
+			out = append(out, temp)
+			temp = nil
+			continue
+		}
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+
+		temp = append(temp, num)
+	}
+
+	return out, nil
+}
+
+func getInput() ([][]int, error) {
+	data, err := os.ReadFile("day1/calories.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	// parse through data
+	lines := strings.Split(string(data), "\n")
+
+	return parseCalories(lines)
+}
+
+func sortedTotalCalories(pack [][]int) []int {
+	var curr int
+	var totals []int
+
+	for _, calories := range pack {
+		for _, c := range calories {
+			curr += c
+		}
+
+		totals = append(totals, curr)
+		curr = 0
+	}
+
+	sort.Slice(totals, func(i, j int) bool {
+		return totals[i] > totals[j]
+	})
+
+	return totals
+}
